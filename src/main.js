4e1dbbfd848dc65e9ba9f55db778d7f4d1fa7bc7
GekaTLT <gekatlt63@mail.ru>
2018-10-02 11:03:14 +0400

    done

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import AppReddit from './App-reddit.vue'
-import VueTouch from 'vue-touch'
 
 import './styles/normalize.css'
-
-Vue.use(VueTouch, { name: 'v-touch' })
 
 Vue.config.productionTip = true
 
